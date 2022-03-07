64d6fda8a2b07cffb953dce3a7bdbdef5c5887be
Greg44DevWeb <ballatgregory44500@gmail.com>
2022-03-07 11:01:25 +0100

    Ajout du système de like/dislike

---
+++ backend/controllers/likes.js
@@ -0,0 +1,71 @@
+//******* CONTROLLER POUR LE LIKE ET DISLIKE */
+
+// import du modèle de Sauce à liker
+const Sauce = require('../models/Sauce');
+
+// export de la fonction like
+exports.likeSauce = (req, res, next) => {
+    console.log('je suis dans le controleur like !');
+    // récupère le champs likes
+    const likeStatus = req.body.like;
+    // récupère l'id de l'URL
+    const sauceId = req.params.id;
+    // récupère le userId
+    const userId = req.body.userId;
+
+    //***  METHODE SWITCH POUR ENUMERER LES CAS A GERER POUR LIKE ET DISLIKE ***/
+    switch(likeStatus) {
+        // ajout d'un like
+        case 1:
+            //***  vérifier qu'il n'y a pas déjà un like avec findOne  ***/
+            Sauce.updateOne({ _id: sauceId}, { 
+                $inc: { likes: +1 }, // operateur qui permet l'incrément de 1 avec la fonction updateOne (doc MongoDb)
+                $push: { usersLiked: req.body.userId } // opérateur mgdb qui inclus la valeur dans le tableau UsersLiked
+            })
+            .then(() => res.status(201).json({ message: 'Ajout du like !'}))
+            .catch((error) => res.status(400).json({ error }));
+            break;
+
+        //**** Ajout d'un Dislike *******/   
+        case -1:
+            Sauce.updateOne({ _id: sauceId}, {
+                $inc: { dislikes: +1 },
+                $push: { usersDisliked: req.body.userId }
+            })
+            .then(() => res.status(201).json({ message: "Ajout d'un dislike ! "}))
+            .catch((error) => res.status(400).json({ error }));
+            break;
+
+        //***  suppression like et dislike ***/   
+        case 0:
+            Sauce.findOne({ _id: sauceId })
+            .then((sauce) => {
+                //Supprimer son like de UsersLiked
+                if(sauce.usersLiked.includes(userId)){
+                    Sauce.updateOne({ _id: sauceId},
+                        {
+                            $inc: { likes: -1 },
+                            $pull: { usersLiked: userId} // Remove la valeur du tableau usersLiked
+                        })
+                    .then(console.log('test --- > Like supprimé'))   
+                    .then(() => res.status(201).json({ message: "like supprimé !"}))
+                    .catch((error) => res.status(400).json({ error }));
+                    
+                } else if(sauce.usersDisliked.includes(userId)) {
+                    // Supprimer son dislike de usersDisliked
+                    Sauce.updateOne({_id: sauceId},
+                        {
+                            $inc: { dislikes: -1}, // Incrémente de -1 la valeur du like
+                            $pull: { usersDisliked: userId} // Remove la valeur du tableau usersDisLiked
+                        })
+                    .then(console.log('test --- > disLike supprimé')) 
+                    .then(() => res.status(201).json({ message: "dislike supprimé ! "}))
+                    .catch((error) => res.status(400).json({ error }));
+                } else {
+                    res.status(403).json({ message: "requête impossible !"})
+                }
+            })
+            .catch(() => res.status(404).json({ message: "Sauce introuvable !"}));
+            break;
+    }
+};
